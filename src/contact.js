5beaf68c6f680af88be684d486b9cc8db9054fd2
Bradley Reynolds <bsreynolds.busi@gmail.com>
2023-04-16 13:08:52 -0500

    Completed project, for now

--- src/contact.js
+++ src/contact.js
@@ -1,6 +1,13 @@
 export const contact = `
                 <section id="contact">
-                    <h1>Contact Page</h1>
-                    <p>Here's how to get in contact with Bardelinni's Pizza</p>
+                    <div class="contact-con">
+                        <div>
+                            <i class="fas fa-phone-alt"></i> <p>219-123-4567</p> <br/>
+                        </div>
+                        <div>
+                            <i class="fas fa-map-pin"></i> <p>2108 S Michigan Ave, Chicago, IL 60616</p>
+                        </div>
+                        <img src="../images/address.png" alt="Address" />
+                    </div>
                 </section>
                 `;
